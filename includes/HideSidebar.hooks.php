627bf527a4b701c1daf18537f5805f7b592debef
Samy Elshamy <elshamy@coderbutze.de>
2025-07-16 12:52:08 +0200

    Fix compatibility with 1.44

--- includes/HideSidebar.hooks.php
+++ includes/HideSidebar.hooks.php
@@ -3,7 +3,7 @@ class HideSidebarHooks {
     public static function efHideSidebar( $skin, &$bar ) {
         // Hide sidebar for anonymous users
         if ( $skin->getUser()->isAnon() ) {
-            $url = Title::makeTitle( NS_SPECIAL, 'UserLogin' )->getLocalUrl();
+            $url = SpecialPage::getTitleFor( 'UserLogin' )->getLocalUrl();
             $bar = [];
             $bar['navigation'][] = [
                 'text'   => 'Login',
